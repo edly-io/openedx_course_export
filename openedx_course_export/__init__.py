3c1cd9f6690aa25ffdbf6f6657afffb32d9583f2
Zia Fazal <zia.fazal@arbisoft.com>
2023-02-28 16:49:03 +0500

    chore: version bump

--- openedx_course_export/__init__.py
+++ openedx_course_export/__init__.py
@@ -2,6 +2,6 @@
 A django app plugin to add a new API to Open edX to export courses to S3 buckets
 """
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 default_app_config = 'openedx_course_export.apps:CourseExportConfig'  # pylint: disable=invalid-name
